88bf2b3273793bb4e8b84c1d3aeb256a731f4060
jjong <whdgh9588@gmail.com>
2024-12-25 16:20:32 +0900

    REFACTOR: 경로수정

--- Epari-board/src/App.jsx
+++ Epari-board/src/App.jsx
@@ -1,23 +1,27 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
-import WritePostPage from './pages/WritePostPage';
 import PostDetailPage from './pages/PostDetailPage';
-import './App.css';
-import './styles/editor.css';
+import WritePostPage from './pages/WritePostPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout><HomePage /></Layout>
-  },
-  {
-    path: "/write",
-    element: <Layout><WritePostPage /></Layout>
-  },
-  {
-    path: "/posts/:postId",
-    element: <Layout><PostDetailPage /></Layout>
+    element: <Layout/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      },
+      {
+        path: "posts/:postId",
+        element: <PostDetailPage/>
+      },
+      {
+        path: "write",
+        element: <WritePostPage/>
+      }
+    ]
   }
 ], {
   future: {
@@ -27,7 +31,7 @@ const router = createBrowserRouter([
 });
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
